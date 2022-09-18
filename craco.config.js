bb1795b8873537508241330e1a1a8baf0b7f88f1
zongzheng123 <304389312@qq.com>
2022-09-18 17:47:01 +0800

    add react dev inspector
    Signed-off-by: zongzheng123 <304389312@qq.com>

--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,33 @@
+const {
+  launchEditorMiddleware,
+} = require("react-dev-inspector/plugins/webpack");
 module.exports = {
-    webpack: {
-        configure:{
-            // See https://github.com/webpack/webpack/issues/6725
-            module:{
-                rules: [{
-                    test: /\.wasm$/,
-                    type: 'javascript/auto',
-                }]
-            },
-            resolve: {
-                extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],// other stuff
-                fallback: {
-                    "fs": false,
-                    "path": require.resolve("path-browserify"),
-                    "crypto": false,
-                }
-            },
+  devServer: (devServerConfig, { env, paths, proxy, allowedHost }) => {
+    devServerConfig.setupMiddlewares = (middlewares, devServer) => {
+      middlewares.unshift(launchEditorMiddleware);
+      return middlewares;
+    };
+    return devServerConfig;
+  },
+  webpack: {
+    configure: {
+      // See https://github.com/webpack/webpack/issues/6725
+      module: {
+        rules: [
+          {
+            test: /\.wasm$/,
+            type: "javascript/auto",
+          },
+        ],
+      },
+      resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"], // other stuff
+        fallback: {
+          fs: false,
+          path: require.resolve("path-browserify"),
+          crypto: false,
         },
-
-    }
-
-
+      },
+    },
+  },
 };
--- src/index.js
+++ src/index.js
@@ -5,16 +5,29 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import { Provider} from 'jotai'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Inspector, InspectParams } from 'react-dev-inspector'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const isDev = process.env.NODE_ENV === 'development'
 root.render(
   <React.StrictMode>
+    {isDev && <Inspector
+     keys={['control', 'shift', 'z']}
+     disableLaunchEditor={false}
+    //  onClickElement={({ codeInfo }) => {
+    //    if (!codeInfo?.absolutePath) return
+    //    const { absolutePath, lineNumber, columnNumber } = codeInfo
+    //    // you can change the url protocol if you are using in Web IDE
+    //    window.open(`vscode://file/${absolutePath}:${lineNumber}:${columnNumber}`)
+    //  }}
+    />}
       <Provider>
           <BrowserRouter>
     <App />
 
           </BrowserRouter>
       </Provider>
+
   </React.StrictMode>
 );
 
